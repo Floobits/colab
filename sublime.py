5dd6f3f06f937a88921f9aecb107e4643f9bcac9
Matt Kanairis <mkaniaris@gmail.com>
2012-09-18 23:59:01 -0700

    sorta working sublime watcher

---
+++ sublime.py
@@ -0,0 +1,45 @@
+import sublime, sublime_plugin
+
+class Listener(sublime_plugin.EventListener):
+	def __init__(self, *args, **kwargs):
+		sublime_plugin.EventListener.__init__(self, *args, **kwargs)
+		self.dirty = False
+		self.t = ""
+		self.sel = None
+		
+	def on_new(self, view):
+		print view
+
+	def on_load(self, view):
+		self.t = self.text(view)
+		print view
+
+	def on_modified(self, view):
+		self.dirty = True
+		print 'dirty'
+		self.t = self.text(view)
+
+	def text(self, view):
+		return view.substr(sublime.Region(0, view.size())).encode('utf-8')
+
+	def on_selection_modified(self, view):
+		sel = view.sel()[0]
+
+		if not self.dirty:
+			self.sel = sel
+			return
+		
+		self.dirty = False
+		
+		if self.sel is None:
+			self.sel = sel
+
+		region = (self.sel.begin(), sel.end())
+		self.sel = sel
+		print region
+		print region, view.substr(sublime.Region(*region))
+		#print view.substr(sublime.Region(sel.begin()-1, sel.end()))
+		
+		# region = view.visible_region()abcde
+		# text = view.substr(region)
+		#lastLine = view.rowcol(sel[0].end())[0]
