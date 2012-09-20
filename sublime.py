8168d7ea11a583a50c1f7918ea1783926a85c933
Matt Kanairis <mkaniaris@gmail.com>
2012-09-20 01:23:09 -0700

    moving stuff to lib dir... still experimenting

--- sublime.py
+++ sublime.py
@@ -1,4 +1,5 @@
 import sublime, sublime_plugin
+from lib import diff_match_patch as dmp 
 
 class Listener(sublime_plugin.EventListener):
 	def __init__(self, *args, **kwargs):
@@ -6,6 +7,7 @@ class Listener(sublime_plugin.EventListener):
 		self.dirty = False
 		self.t = ""
 		self.sel = None
+		self.patch = dmp.diff_match_patch().patch_make
 		
 	def on_new(self, view):
 		print view
@@ -16,8 +18,10 @@ class Listener(sublime_plugin.EventListener):
 
 	def on_modified(self, view):
 		self.dirty = True
-		print 'dirty'
-		self.t = self.text(view)
+		# print 'dirty'
+		# self.t = self.text(view)
+		print self.patch(self.text(view), self.text(self.view))
+		self.view = view
 
 	def text(self, view):
 		return view.substr(sublime.Region(0, view.size())).encode('utf-8')
@@ -25,19 +29,19 @@ class Listener(sublime_plugin.EventListener):
 	def on_selection_modified(self, view):
 		sel = view.sel()[0]
 
-		if not self.dirty:
-			self.sel = sel
-			return
+		# if not self.dirty:
+		# 	self.sel = sel 
+		# 	return
 		
-		self.dirty = False
+		# self.dirty = False
 		
-		if self.sel is None:
-			self.sel = sel
+		# if self.sel is None:
+		# 	self.sel = sel
 
-		region = (self.sel.begin(), sel.end())
-		self.sel = sel
-		print region
-		print region, view.substr(sublime.Region(*region))
+		# region = (self.sel.begin(), sel.end())
+		# self.sel = sel
+		# print region
+		# print region, view.substr(sublime.Region(*region))
 		#print view.substr(sublime.Region(sel.begin()-1, sel.end()))
 		
 		# region = view.visible_region()abcde
