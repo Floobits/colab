08746ac30eb1d761e920a63216005e2b80b9b13d
Matt Kaniaris <mkaniaris@gmail.com>
2012-09-28 18:07:43 -0700

    Adding ignore, package.json, and updated server to length prefixed 20 byte strings.

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 
 var net = require('net');
 
+var _ = require('underscore');
+
+var active_conns = {};
+var conn_number = 0;
+var LENGTH_PREFIX = 20;
+
 var server = net.createServer(function(c) {
+  var number = ++conn_number;
+  var has_authed = false;
+  var buf = "";
+  
+  active_conns[number] = c;
+
   console.log('server connected');
+
   c.on('end', function() {
+    delete active_conns[number];
     console.log('server disconnected');
   });
+
   c.on('data', function(d){
-    console.log(d.toString());
+    var length_chars, length, msg;
+    buf += d;
+    if (buf.length < LENGTH_PREFIX){
+      return;
+    }
+    length_chars = buf.slice(0, LENGTH_PREFIX);
+    if (buf.length < length_chars + LENGTH_PREFIX){
+      return;
+    }
+    msg = buf.slice(0, length_chars+LENGTH_PREFIX);
+    buf = buf.slice(length_chars+LENGTH_PREFIX);
+    console.log(msg);
+    _.each(active_conns, function(conn, id){
+      if (id === number){
+        return;
+      }
+      conn.write(msg);
+    });
   });
-  c.pipe(c);
 });
 server.listen(3148);
