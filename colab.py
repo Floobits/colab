10294af0ad07405188090ae840b70a92165a7321
Matt Kaniaris <mkaniaris@gmail.com>
2012-09-30 23:00:50 -0700

    Ripping out pieces of the nodejs sublime plugin.  We now have a working menu/settings and a command to run that dumps hawro into the panel and as a flash message.

--- colab.py
+++ colab.py
@@ -5,6 +5,8 @@ import threading
 import time
 import socket
 import asyncore
+import os
+import sys
 
 import sublime
 import sublime_plugin
@@ -50,8 +52,12 @@ class Conn(asyncore.dispatcher):
        # self.close()
         unrun()
 
-    def handle_error(self, err):
-        print(err)
+    def handle_error(self, err=None):
+        if err:
+            print(err)
+            return
+        print 'got error'
+
 
     def handle_read(self):
         self.buffer_in += self.recv(1024)
@@ -84,10 +90,8 @@ class Conn(asyncore.dispatcher):
         view.replace(region, t)
 
     def send_patches(self):
-        print 'calling send patches'
         reported = set()
         while (True):
-            print 'in send patches'
             try:
                 view = Q.get_nowait()
             except Queue.Empty:
@@ -166,10 +170,77 @@ class Listener(sublime_plugin.EventListener):
         return True
 
 
+class JoinChannelCommand(sublime_plugin.TextCommand):
+    def run(self, *args, **kwargs):
+        sublime.status_message('hawro')
+        #self.panel('hawro')
+
+    def active_view(self):
+        return self.view
+
+    def is_enabled(self):
+        # First, is this actually a file on the file system?
+        if self.view.file_name() and len(self.view.file_name()) > 0:
+            return os.path.realpath(self.get_working_dir())
+
+    def get_file_name(self):
+        return os.path.basename(self.view.file_name())
+
+    def get_working_dir(self):
+        return os.path.dirname(self.view.file_name())
+
+    def get_window(self):
+        # Fun discovery: if you switch tabs while a command is working,
+        # self.view.window() is None. (Admittedly this is a consequence
+        # of my deciding to do async command processing... but, hey,
+        # got to live with that now.)
+        # I did try tracking the window used at the start of the command
+        # and using it instead of view.window() later, but that results
+        # panels on a non-visible window, which is especially useless in
+        # the case of the quick panel.
+        # So, this is not necessarily ideal, but it does work.
+        return self.view.window() or sublime.active_window()
+
+    def generic_done(self, result):
+        if not result.strip():
+            return
+        self.panel(result)
+
+    def _output_to_view(self, output_file, output, clear=False, syntax="Packages/JavaScript/JavaScript.tmLanguage"):
+        output_file.set_syntax_file(syntax)
+        edit = output_file.begin_edit()
+        if clear:
+            region = sublime.Region(0, self.output_view.size())
+            output_file.erase(edit, region)
+        output_file.insert(edit, 0, output)
+        output_file.end_edit(edit)
+
+    def scratch(self, output, title=False, **kwargs):
+        scratch_file = self.get_window().new_file()
+        if title:
+            scratch_file.set_name(title)
+        scratch_file.set_scratch(True)
+        self._output_to_view(scratch_file, output, **kwargs)
+        scratch_file.set_read_only(True)
+        return scratch_file
+
+    def panel(self, output, **kwargs):
+        if not hasattr(self, 'output_view'):
+            self.output_view = self.get_window().get_output_panel("git")
+        self.output_view.set_read_only(False)
+        self._output_to_view(self.output_view, output, clear=True, **kwargs)
+        self.output_view.set_read_only(True)
+        self.get_window().run_command("show_panel", {"panel": "output.git"})
+
+    def quick_panel(self, *args, **kwargs):
+        self.get_window().show_quick_panel(*args, **kwargs)
+
+
 def unrun():
     global active
     active = False
-    raise KeyboardInterrupt('time to die')
+    print('bailing')
+    sys.exit(1)
 
 
 def run():
