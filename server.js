75c4844d93bde5432ebf2857609c3a3d108ac59d
Matt Kaniaris <mkaniaris@gmail.com>
2012-10-02 21:53:07 -0700

    Abstracting Backend Server
    We now have a ColabServer and AgentConnection.  The server extends net.Server while AC extends events.EventEmitter.  I have also introduced the concept of rooms.  The handshake and disconnects if invalid need to be handled better.  We should really settle on a protocol.

--- server.js
+++ server.js
@@ -1,49 +1,111 @@
 
 var net = require('net');
+var util = require('util');
+var events = require('events');
 
 var _ = require('underscore');
 
-var active_conns = {};
-var conn_number = 0;
 var LENGTH_PREFIX = 20;
 
-var server = net.createServer(function(c) {
-  var number = ++conn_number;
-  var has_authed = false;
-  var buf = "";
+var ColabServer = function(){
+  var self = this;
+  self.number = 0;
+  self.agents = {};
+  self.channels = {};
+};
 
-  active_conns[number] = c;
+util.inherits(ColabServer, net.Server);
 
-  console.log('server connected');
+ColabServer.prototype.listen = function(port, address){
+  var self = this;
+  net.Server.call(self, self.on_conn.bind(self));
+  net.Server.prototype.listen.call(self, port, address);
+  console.log('now listening on port: ' + port);
+};
 
-  c.on('end', function() {
-    delete active_conns[number];
-    console.log('server disconnected');
-  });
+ColabServer.prototype.on_conn = function(conn){
+  var self = this;
+  var number = ++self.conn_number;
+  var agent = new AgentConnection(number, conn, self);
+  self.agents[number] = number;
+  agent.once('on_conn_end', self.on_conn_end.bind(self));
+};
 
-  c.on('data', function(d){
-    var length_chars, length, msg;
-    console.log("d: " + d);
-    buf += d;
-    console.log("buf: " + buf);
-    if (buf.length < LENGTH_PREFIX){
-      console.log("getting prefix: buf is only " + buf.length + " bytes");
-      return;
+ColabServer.prototype.on_conn_end = function(agent){
+  var self = this;
+  delete self.agents[agent.id];
+  console.log('server disconnected');
+};
+
+ColabServer.prototype.broadcast = function(room, dpm){
+  var self = this;
+  _.each(self.agents, function(agent){
+    if (agent.attending(room)){
+      agent.send_dmp(dpm);
     }
-    length_chars = parseInt(buf.slice(0, LENGTH_PREFIX), 10);
-    if (buf.length < length_chars + LENGTH_PREFIX) {
-      console.log("getting msg: buf is only " + buf.length + " bytes. want " + length_chars + LENGTH_PREFIX + " bytes");
-      return;
-    }
-    msg = buf.slice(0, length_chars+LENGTH_PREFIX);
-    buf = buf.slice(length_chars+LENGTH_PREFIX);
-    console.log(msg);
-    _.each(active_conns, function(conn, id){
-      if (id === number){
-        return;
-      }
-      conn.write(msg);
-    });
   });
-});
+};
+
+var AgentConnection = function(id, conn, server){
+  var self = this;
+  self.id = id;
+  self.conn = conn;
+  self.server = self;
+  self.buf = "";
+  self.rooms = [];
+  conn.on('end', function(){
+    self.emit('on_conn_end', self);
+  });
+  conn.on('data', self.on_data.bind(self));
+  self.on('requst', self.on_request.bind(self));
+  events.EventEmitter.call(self);
+};
+
+util.inherits(AgentConnection, events.EventEmitter);
+
+AgentConnection.prototype.send_dmp = function(dmp){
+  var self = this;
+  if (self.conn){
+    self.conn.write(dmp);
+  }
+};
+
+AgentConnection.prototype.attending = function(room){
+  // easy and slow!
+  var self = this;
+  return _.has(self.rooms, room);
+};
+
+AgentConnection.prototype.on_data = function(d){
+  var self = this;
+  var length_chars, length, msg;
+
+  console.log("d: " + d);
+
+  self.buf += d;
+
+  if (self.buf.length < LENGTH_PREFIX){
+    console.log("getting prefix: buf is only " + self.buf.length + " bytes");
+    return;
+  }
+
+  length_chars = parseInt(self.buf.slice(0, LENGTH_PREFIX), 10);
+  if (self.buf.length < length_chars + LENGTH_PREFIX) {
+    console.log("getting msg: buf is only " + self.buf.length + " bytes. want " + length_chars + LENGTH_PREFIX + " bytes");
+    return;
+  }
+
+  msg = self.buf.slice(LENGTH_PREFIX, length_chars+LENGTH_PREFIX);
+  self.emit('request', msg);
+  self.buf = self.buf.slice(length_chars+LENGTH_PREFIX);
+};
+
+AgentConnection.prototype.on_request = function(raw){
+  var self = this;
+  raw = raw.slice(LENGTH_PREFIX);
+  json = JSON.parse(raw);
+  console.log(json);
+};
+
+var server = new ColabServer();
 server.listen(3148);
