5e5fd407920e633aea1dc202a1feb1a27b7ef5b2
Geoff Greer <geoff@greer.fm>
2012-10-22 12:44:44 -0700

    Don't explode and die on any exception

--- backend/lib/entry.js
+++ backend/lib/entry.js
@@ -4,6 +4,10 @@ var optimist = require('optimist');
 var path = require('path');
 var fs = require('fs');
 
+process.on('uncaughtException', function(err) {
+  console.error(err.stack);
+});
+
 exports.run = function run() {
   require('./server').run();
 };
