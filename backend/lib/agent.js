94a973c7c1575a11c3fd34821391f6b35c627cdb
Geoff Greer <geoff@greer.fm>
2012-10-22 11:56:27 -0700

    Progress. Getting closer to storing all buffers in memory and keeping track of patches applied and whatnot

--- backend/lib/agent.js
+++ backend/lib/agent.js
@@ -71,12 +71,6 @@ AgentConnection.prototype.send_dmp = function(dmp){
   }
 };
 
-AgentConnection.prototype.attending = function(room){
-  // easy and slow!
-  var self = this;
-  return _.has(self.rooms, room);
-};
-
 AgentConnection.prototype.on_data = function(d){
   var self = this;
   var msg;
@@ -95,22 +89,32 @@ AgentConnection.prototype.on_data = function(d){
   msg = msg[0];
 
   if (self.authenticated) {
-      _.each(self.room.agents, function (v, k) {
-        log.debug("agent" + v.id + " self " + self.id);
-        if (v.id === self.id) {
-          return;
-        }
-        v.conn.write(msg + "\n");
-      });
-    //  self.emit('request', msg);
+    self.emit('request', msg);
   } else {
     auth_data = JSON.parse(msg);
     if (_.has(auth_data, "username") &&
         _.has(auth_data, "secret") &&
-        _.has(auth_data, "room")) {
+        _.has(auth_data, "room") &&
+        _.has(auth_data, "version")) {
+      if (!_.contains(SUPPORTED_VERSIONS, auth_data.version)){
+        log.log("unsupported client version. disconnecting");
+        self.conn.destroy();
+        return;
+      }
+
       self.username = auth_data.username;
       self.secret = auth_data.secret;
       self.room = auth_data.room;
+
+      if (!_.has(self.server.rooms, auth_data.room)) {
+        self.server.rooms[auth_data.room] = {
+          agents: {},
+          bufs: {}
+        };
+      }
+      self.room = self.server.rooms[auth_data.room];
+      self.room.agents[self.id] = self;
+      self.bufs = self.room.bufs;
       /* todo: actually auth against something */
       self.authenticated = true;
       log.debug("client authenticated. yay!");
@@ -118,33 +122,38 @@ AgentConnection.prototype.on_data = function(d){
       log.log("bath auth json. disconnecting client");
       /* TODO: cancel interval for disconnect_unauthed_client */
       self.conn.destroy();
+      return;
     }
   }
 };
 
 AgentConnection.prototype.on_request = function(raw){
   var self = this;
-  var req, buf;
-  log.debug(raw);
-  req = JSON.parse(raw);
-  if (!req.v || !_.has(SUPPORTED_VERSIONS, req.v)){
-    log.log("bad client. goodbye");
-//    return self.conn.destroy();
+  var buf;
+  var req = JSON.parse(raw);
+
+  /*
+  // Interim hack
+  _.each(self.room.agents, function (v, k) {
+    log.debug("agent", v.id, "self", self.id);
+    if (v.id === self.id) {
+      return;
+    }
+    v.conn.write(raw + "\n");
+  });
+*/
+  if (!req.path) {
+    log.log("bad client: no path. goodbye");
+    return self.conn.destroy();
   }
 
-  if (!req.event.uid){
-    log.log("bad client: no event uid. goodbye");
-//    return self.conn.destroy();
+  buf = self.bufs[req.path];
+  if (buf) {
+    buf.emit(req.path, req);
+  } else {
+    buf = new ColabBuffer(self, req.path, req.patch);
+    self.bufs[buf.path] = buf;
   }
-
-  buf = self.colab_bufs[req.event.uid];
-  if (buf){
-    return buf.emit(req.event, req);
-  }
-
-  buf = new ColabBuffer(self, req.uid, req.name, req.patches);
-
-  self.colab_bufs[buf.uid] = buf;
 };
 
 AgentConnection.prototype.on_dmp = function(json){
--- backend/lib/buffer.js
+++ backend/lib/buffer.js
@@ -5,10 +5,10 @@ var events = require('events');
 var DMP = new require('diff_match_patch').diff_match_patch();
 var _ = require('underscore');
 
-var ColabBuffer = function(agent, uid, name, patches){
+var ColabBuffer = function(agent, path, patch){
   var self = this;
-  self.guid = util.format("%s-%s", agent.id, uid);
-  self.name = name;
+  self.guid = util.format("%s-%s", agent.id, path);
+  self.path = path;
   self._agent = agent;
   self._state = "";
   self._checksum = null;
@@ -17,20 +17,22 @@ var ColabBuffer = function(agent, uid, name, patches){
 
 util.inherits(ColabBuffer, events.EventEmitter);
 
-ColabBuffer.prototype.on_dmp = function(patches, checksum){
+ColabBuffer.prototype.on_dmp = function(patch, checksum){
   if (!self._is_valid){
+    log.error("buffer is no longer valid because we got out of sync earlier. FROWNY FACE :(");
     return;
   }
   if (checksum === self._checksum){
     return;
   }
   // probably should adjust some nobs or something?
-  DMP.patch_apply(patches, self._state);
+  DMP.patch_apply(patch, self._state);
   var hash = crypto.createHash('md5').update(self._state);
 
   if (hash.digest("hex") !== checksum){
     // TODO- tell client to resend whole damn file
     self._is_valid = false;
+    log.error("checksum doesn't match! we should re-request the file but we don't");
     return;
   }
   self.checksum = checksum;
@@ -38,9 +40,9 @@ ColabBuffer.prototype.on_dmp = function(patches, checksum){
   // tell our agent
   self._agent.emit('dmp', {
     checksum: self.checksum,
-    id: self.guid,
-    name: self.name,
-    dmp: patches
+    guid: self.guid,
+    path: self.path,
+    patch: patch
   });
 };
 
