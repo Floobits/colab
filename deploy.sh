079bdea7b62bad4c7f0faab4965f298118006924
Geoff Greer <geoff@greer.fm>
2012-11-01 18:00:32 -0700

    Exclude node modules. npm install before restarting service

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@
 DATA_DIR=/data
 #DATA_DIR=/data-staging
 
-rsync -avz . floobits.com:$DATA_DIR/colab
+rsync -avz --exclude=node_modules . floobits.com:$DATA_DIR/colab
+
+ssh floobits.com "cd $DATA_DIR/colab/ && npm install"
+
 ssh floobits.com "sudo sv restart /service/colab/"
