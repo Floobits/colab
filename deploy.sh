06346bfa2e2ef46e84c7ae8cf0093ff59cd686b0
Geoff Greer <geoff@greer.fm>
2012-11-15 14:07:27 -0800

    Add deps. Better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-DATA_DIR=/data
-#DATA_DIR=/data-staging
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 hostname"
+  exit 0
+fi
 
-rsync -avz --exclude=node_modules . floobits.com:$DATA_DIR/colab
+rsync -avz --exclude=node_modules . $1:/data/colab
 
-ssh floobits.com "cd $DATA_DIR/colab/ && npm install"
-
-ssh floobits.com "sudo sv restart /service/colab/"
+ssh $1 "cd /data/colab/ && npm install"
+ssh $1 "sudo sv restart /service/colab/"
