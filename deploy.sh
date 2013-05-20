7fa374ed632e0d8809c3bcf9b528bdb3120617d1
Geoff Greer <geoff@greer.fm>
2013-05-20 13:23:53 -0700

    Deploy script that uses symlinks

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,18 @@ then
   exit 0
 fi
 
-rsync -avz --exclude=node_modules --exclude=lib/settings.js . $1:/data/colab
+DATETIME=`date -u '+%Y_%m_%d_%H%M'`
+RELEASE_NAME="colab-$DATETIME"
 
-ssh $1 "cd /data/colab/ && npm install && sudo sv restart /service/colab/"
+tar -c -z -f $RELEASE_NAME.tar.gz `git ls-files`
+
+scp -C $RELEASE_NAME.tar.gz $1:/tmp
+
+ssh $1 "sudo mkdir /data/releases/$RELEASE_NAME && sudo tar xzf /tmp/$RELEASE_NAME.tar.gz --directory /data/releases/$RELEASE_NAME"
+ssh $1 "sudo cp /data/colab/lib/settings.js /data/releases/$RELEASE_NAME/lib/settings.js"
+ssh $1 "sudo cp -r /data/colab/node_modules /data/releases/$RELEASE_NAME/"
+ssh $1 "cd /data/releases/$RELEASE_NAME && \
+sudo npm install && \
+sudo ln -s -f /data/releases/$RELEASE_NAME /data/colab-new && \
+sudo mv -T -f /data/colab-new /data/colab && \
+sudo sv restart /service/colab/"
