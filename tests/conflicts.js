30d3a4260503d6106a6abb86bb5cbc897b13ca8f
Geoff Greer <geoff@greer.fm>
2013-06-06 16:33:27 -0700

    New tests

--- tests/conflicts.js
+++ tests/conflicts.js
@@ -53,6 +53,32 @@ var test3 = function (test) {
   test.done();
 };
 
+var test4 = function(test){
+  agent1.buf = "abc";
+  agent2.buf = "abc";
+
+  patch(agent1, "abcde");
+  patch(agent2, "abcdef");
+  agent2.pop_patch(-1);
+  agent1.pop_patch(-1);
+
+  verify(test, [agent1, agent2]);
+  test.done();
+}
+
+var test5 = function(test){
+  agent1.buf = "abc";
+  agent2.buf = "abc";
+
+  patch(agent1, "ab");
+  patch(agent2, "abcdef");
+  agent2.pop_patch(-1);
+  agent1.pop_patch(-1);
+
+  verify(test, [agent1, agent2]);
+  test.done();
+}
+
 
 module.exports = {
   setUp: test.setup,
@@ -60,4 +86,6 @@ module.exports = {
   test1: test1,
   test2: test2,
   test3: test3,
+  test4: test4,
+  test5: test5
 };
