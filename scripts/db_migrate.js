5f75dc543077ddc1cf26fffb009562bc7d31116c
Geoff Greer <geoff@greer.fm>
2013-12-18 00:48:46 -0800

    More logging. Figure out if a buffer is missing from s3 or wrong on disk or whatever.

--- scripts/db_migrate.js
+++ scripts/db_migrate.js
@@ -41,6 +41,8 @@ var load_s3 = function (key, cb) {
 
 var stats = {
   bufs: {
+    bad_checksum: 0,
+    empty: 0,
     failed: 0,
     total: 0
   },
@@ -52,19 +54,22 @@ var stats = {
 
 var final_stats = function (err) {
   var succeeded = (stats.bufs.total - stats.bufs.failed);
+  console.log("\n");
   if (err) {
     console.error("Error:");
     console.error(err);
     console.error("Stack:");
     console.error(err.stack);
   }
+  console.log("\n");
   log.log("Migrated %s/%s buffers (%d%)", succeeded, stats.bufs.total, (succeeded / stats.bufs.total) * 100);
   succeeded = (stats.workspaces.total - stats.workspaces.failed);
+  log.log("%s bad checksums (%d%)", stats.bufs.bad_checksum, (stats.bufs.bad_checksum / stats.bufs.total) * 100);
+  log.log("%s empty (%d%)", stats.bufs.empty, (stats.bufs.empty / stats.bufs.total) * 100);
   log.log("Migrated %s/%s workspaces (%d%)", succeeded, stats.workspaces.total, (succeeded / stats.workspaces.total) * 100);
   process.exit();
 };
 
-process.on("exit", final_stats);
 process.on("uncaughtException", final_stats);
 process.on("SIGINT", final_stats);
 process.on("SIGTERM", final_stats);
@@ -168,7 +173,11 @@ var migrate_room = function (db_room, cb) {
                 checksum_matches++;
                 return cb(null, true);
               }
+              if (buf.md5 === null || response.buf_content_get === "") {
+                return cb(null, false);
+              }
               log.warn("MD5 mismatch when loading %s! Was %s. Should be %s.", buf.fid, buf_md5, buf.md5);
+              stats.bufs.bad_checksum++;
             }
             return cb(null, false);
           }];
@@ -183,12 +192,21 @@ var migrate_room = function (db_room, cb) {
             });
             buf_path = path.join(room_path, buf.fid.toString());
             fs.readFile(buf_path, function (err, result) {
+              var buf_md5;
               if (err) {
                 log.info("Error reading %s: %s.", buf_path, err);
                 return cb(null, false);
               }
-              save_buf_content(ws, buf, result);
-              return cb(null, true);
+              buf_md5 = utils.md5(response.buf_content_get);
+              if (buf_md5 === buf.md5) {
+                save_buf_content(ws, buf, result);
+                return cb(null, true);
+              }
+              if (buf.md5 === null || response.buf_content_get === "") {
+                return cb(null, false);
+              }
+              log.error("MD5 mismatch when loading off disk %s! Was %s. Should be %s.", buf.fid, buf_md5, buf.md5);
+              return cb(null, false);
             });
           }];
 
@@ -201,6 +219,7 @@ var migrate_room = function (db_room, cb) {
             load_s3(s3_key, function (err, result) {
               if (err) {
                 log.error("Error reading %s from s3: %s", s3_key, err);
+                stats.bufs.empty++;
                 save_buf_content(ws, buf, "");
                 cb(err);
                 return;
@@ -235,12 +254,14 @@ db.query("SELECT * FROM room_room", function (err, result) {
   }
 
   async.eachLimit(result.rows, 5, function (db_room, cb) {
-    log.log("Migrating %s", db_room.id);
+    log.debug("Migrating %s", db_room.id);
     migrate_room(db_room, cb);
   }, function (err) {
+    console.log("\n");
     if (err) {
       log.error("Error: %s", err);
     }
+    console.log("\n");
     log.log("%s workspaces in DB", result.rows.length);
     final_stats();
   });
