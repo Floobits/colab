034223c0f0fddbaab434e50988c813152fc192c6
Geoff Greer <geoff@greer.fm>
2014-01-16 15:38:01 -0800

    Start cleaning up migration script.

--- scripts/db_migrate.js
+++ scripts/db_migrate.js
@@ -96,215 +96,203 @@ var save_buf_content = function (ws, buf, value) {
 };
 
 var migrate_room = function (server_db, db_room, cb) {
-  db.query("SELECT * FROM room_buffer WHERE room_id = $1 AND deleted = FALSE", [db_room.id], function (err, result) {
-    var checksum_matches = 0,
-      db_path,
-      room_path;
-
-    room_path = path.normalize(path.join(settings.base_dir, "bufs", db_room.id.toString()));
+  var auto = {},
+    room_path = path.normalize(path.join(settings.base_dir, "bufs", db_room.id.toString())),
     db_path = path.join(room_path, "db");
-    stats.workspaces.total++;
 
+  auto.db_bufs = function (cb) {
+    db.query("SELECT * FROM room_buffer WHERE room_id = $1 AND deleted = FALSE", [db_room.id], cb);
+  };
+
+  auto.mkdir = function (cb) {
+    stats.workspaces.total++;
+    fs.mkdirs(room_path, cb);
+  };
+
+  auto.ldb = ["mkdir", function (cb) {
+    levelup(db_path, { valueEncoding: "json" }, cb);
+  }];
+
+  auto.migrate_bufs = ["db_bufs", "ldb", function (cb, res) {
+    var checksum_matches = 0,
+      db_bufs = res.db_bufs,
+      ldb = res.ldb,
+      ws;
+
+    cb = _.once(cb);
+
+    log.log("%s (%s): migrating %s buffers", db_room.name, room_path, db_bufs.rows.length);
+    ws = ldb.createWriteStream();
+    ws.on("close", function () {
+      log.debug("Closed db %s", room_path);
+      ldb.close(function () {
+        setImmediate(function () {
+          cb(null, checksum_matches);
+        });
+      });
+    });
+
+    ws.on("error", function (err) {
+      log.error("Error in db %s: %s", room_path, err);
+      if (!ldb.isClosed()) {
+        ldb.close(function () {
+          stats.workspaces.failed++;
+          setImmediate(function () { cb(err); });
+        });
+      }
+    });
+
+    async.eachLimit(db_bufs.rows, 5, function (buf, cb) {
+      var buf_auto,
+        buf_key = util.format("buf_%s", buf.fid),
+        buf_content_key = util.format("buf_content_%s", buf.fid),
+        buf_obj,
+        buf_path,
+        s3_key;
+
+      buf_obj = {
+        id: buf.fid,
+        path: buf.path,
+        deleted: buf.deleted,
+        md5: buf.md5,
+        encoding: buf.encoding
+      };
+
+      stats.bufs.total++;
+
+      buf_auto = {};
+
+      buf_auto.buf_get = function (cb) {
+        ldb.get(buf_key, function (err, result) {
+          if (err && err.type !== "NotFoundError") {
+            return cb(err, result);
+          }
+          return cb(null, result);
+        });
+      };
+
+      buf_auto.buf_content_get = function (cb) {
+        ldb.get(buf_content_key, { valueEncoding: "binary" }, function (err, result) {
+          if (err && err.type !== "NotFoundError") {
+            return cb(err, result);
+          }
+          return cb(null, result);
+        });
+      };
+
+      buf_auto.verify_buf = ["buf_get", "buf_content_get", function (cb, response) {
+        var buf_md5;
+        if (!response.buf_get) {
+          return cb(null, false);
+        }
+        if (buf.md5 === response.buf_get.md5) {
+          if (_.isUndefined(response.buf_content_get)) {
+            log.warn("No data in buffer %s! Setting to empty.", buf.fid);
+            response.buf_content_get = new Buffer(0);
+          }
+          buf_md5 = utils.md5(response.buf_content_get);
+          if (buf_md5 === buf.md5) {
+            return cb(null, true);
+          }
+          if (buf.md5 === null || response.buf_content_get === "") {
+            return cb(null, false);
+          }
+          log.warn("MD5 mismatch when loading %s (%s)! Was %s. Should be %s.", buf.fid, buf.path, buf_md5, buf.md5);
+          stats.bufs.bad_checksum++;
+        }
+        return cb(null, false);
+      }];
+
+      buf_auto.read_buf = ["verify_buf", function (cb, response) {
+        if (response.verify_buf) {
+          return cb();
+        }
+        ws.write({
+          key: buf_key,
+          value: buf_obj
+        });
+        buf_path = path.join(room_path, buf.fid.toString());
+        fs.readFile(buf_path, function (err, result) {
+          var buf_md5;
+
+          if (err) {
+            log.info("Error reading %s: %s.", buf_path, err);
+            return cb(null, false);
+          }
+          buf_md5 = utils.md5(result);
+          if (buf_md5 === buf.md5) {
+            save_buf_content(ws, buf, result);
+            if (response.buf_content_get) {
+              log.warn("lengths: db: %s file: %s", _.size(response.buf_content_get), _.size(result));
+            }
+            return cb(null, true);
+          }
+          if (response.buf_content_get && buf_md5 === utils.md5(response.buf_content_get)) {
+            log.warn("File and leveldb agree, but postgres doesn't.");
+            return cb(null, true);
+          }
+          if (buf.md5 === null || result === "") {
+            return cb(null, false);
+          }
+          log.error("MD5 mismatch when loading %s %s off disk! Was %s. Should be %s.", buf.fid, buf.path, buf_md5, buf.md5);
+          if (response.buf_content_get) {
+            log.warn("lengths: db: %s file: %s", _.size(response.buf_content_get), _.size(result));
+          }
+          return cb(null, false);
+        });
+      }];
+
+      buf_auto.read_s3 = ["read_buf", function (cb, response) {
+        if (response.read_buf || response.verify_buf) {
+          checksum_matches++;
+          return cb();
+        }
+        if (!s3_client) {
+          stats.bufs.empty++;
+          save_buf_content(ws, buf, "");
+          return cb();
+        }
+        s3_key = util.format("%s/%s", db_room.id, buf.fid);
+        log.debug("Fetching %s from s3.", s3_key);
+        load_s3(s3_key, function (err, result) {
+          if (err) {
+            log.error("Error reading %s from s3: %s", s3_key, err);
+            stats.bufs.empty++;
+            save_buf_content(ws, buf, "");
+          } else {
+            save_buf_content(ws, buf, result);
+          }
+          cb(err);
+        });
+      }];
+
+      async.auto(buf_auto, function (err) {
+        if (err) {
+          stats.bufs.failed++;
+        }
+        cb();
+      });
+    }, function (err) {
+      var version_key = util.format("version_%s", db_room.id);
+      if (err) {
+        server_db.put(version_key, -1, function () {
+          ws.end();
+        });
+      } else {
+        server_db.put(version_key, checksum_matches, function () {
+          ws.end();
+        });
+      }
+    });
+  }];
+
+  async.auto(auto, function (err, result) {
     if (err) {
       log.error("error getting buffers for room", db_room.id, err);
       stats.workspaces.failed++;
       setImmediate(function () { cb(err, result); });
       return;
     }
-
-    fs.mkdirs(room_path, function (err) {
-      if (err) {
-        stats.workspaces.failed++;
-        setImmediate(function () { cb(err); });
-        return;
-      }
-      levelup(db_path, { valueEncoding: "json" }, function (err, ldb) {
-        var ws;
-        if (err) {
-          stats.workspaces.failed++;
-          setImmediate(function () { cb(err); });
-          return;
-        }
-        log.log("%s (%s): migrating %s buffers", db_room.name, room_path, result.rows.length);
-
-        ws = ldb.createWriteStream();
-
-        ws.on("close", function () {
-          log.debug("Closed db %s", room_path);
-          ldb.close(function () {
-            setImmediate(cb);
-          });
-        });
-
-        ws.on("error", function (err) {
-          log.error("Error in db %s: %s", room_path, err);
-          if (!ldb.isClosed()) {
-            ldb.close(function () {
-              stats.workspaces.failed++;
-              setImmediate(function () { cb(err); });
-            });
-          }
-        });
-
-        async.eachLimit(result.rows, 5, function (buf, cb) {
-          var auto,
-            buf_key = util.format("buf_%s", buf.fid),
-            buf_content_key = util.format("buf_content_%s", buf.fid),
-            buf_obj,
-            buf_path,
-            db_encoding = db.buf_encodings_mapping[buf.encoding] === "utf8" ? "utf8" : "binary",
-            s3_key;
-
-          buf_obj = {
-            id: buf.fid,
-            path: buf.path,
-            deleted: buf.deleted,
-            md5: buf.md5,
-            encoding: buf.encoding
-          };
-
-          stats.bufs.total++;
-
-          auto = {};
-
-          auto.buf_get = function (cb) {
-            ldb.get(buf_key, function (err, result) {
-              if (err && err.type !== "NotFoundError") {
-                return cb(err, result);
-              }
-              return cb(null, result);
-            });
-          };
-
-          auto.buf_content_get = function (cb) {
-            ldb.get(buf_content_key, { valueEncoding: db_encoding }, function (err, result) {
-              if (err && err.type !== "NotFoundError") {
-                return cb(err, result);
-              }
-              return cb(null, result);
-            });
-          };
-
-          auto.verify_buf = ["buf_get", "buf_content_get", function (cb, response) {
-            var buf_md5;
-            if (!response.buf_get) {
-              return cb(null, false);
-            }
-            if (buf.md5 === response.buf_get.md5) {
-              if (_.isUndefined(response.buf_content_get)) {
-                log.warn("No data in buffer %s! Setting to empty.", buf.fid);
-                response.buf_content_get = new Buffer(0);
-              }
-              buf_md5 = utils.md5(response.buf_content_get);
-              if (buf_md5 === buf.md5) {
-                return cb(null, true);
-              }
-              if (buf.md5 === null || response.buf_content_get === "") {
-                return cb(null, false);
-              }
-              log.warn("MD5 mismatch when loading %s (%s)! Was %s. Should be %s.", buf.fid, buf.path, buf_md5, buf.md5);
-              // log.warn("Buf encoding: %s. Leveldb encoding: %s.", buf.encoding, db_encoding);
-              stats.bufs.bad_checksum++;
-            }
-            return cb(null, false);
-          }];
-
-          auto.read_buf = ["verify_buf", function (cb, response) {
-            if (response.verify_buf) {
-              return cb();
-            }
-            ws.write({
-              key: buf_key,
-              value: buf_obj
-            });
-            buf_path = path.join(room_path, buf.fid.toString());
-            fs.readFile(buf_path, function (err, result) {
-              var buf_md5,
-                patches;
-              if (err) {
-                log.info("Error reading %s: %s.", buf_path, err);
-                return cb(null, false);
-              }
-              if (db_encoding === "utf8") {
-                buf_md5 = utils.md5(result);
-                // patches = DMP.patch_make(result, response.buf_content_get);
-                result = result.toString(db_encoding);
-                if (buf_md5 !== utils.md5(result)) {
-                  log.error("md5sum changed from %s to %s because of encoding %s", buf_md5, utils.md5(result), db_encoding);
-                  // log.log("START DIFF");
-                  // _.each(patches, function (patch) {
-                  //   log.log(patch.toString());
-                  // });
-                  // log.log("END DIFF");
-                }
-              }
-              buf_md5 = utils.md5(result);
-              if (buf_md5 === buf.md5) {
-                save_buf_content(ws, buf, result);
-                if (response.buf_content_get) {
-                  log.warn("lengths: db: %s file: %s", _.size(response.buf_content_get), _.size(result));
-                }
-                return cb(null, true);
-              }
-              if (response.buf_content_get && buf_md5 === utils.md5(response.buf_content_get)) {
-                log.warn("File and leveldb agree, but postgres doesn't.");
-                return cb(null, true);
-              }
-              if (buf.md5 === null || result === "") {
-                return cb(null, false);
-              }
-              log.error("MD5 mismatch when loading %s %s off disk! Was %s. Should be %s.", buf.fid, buf.path, buf_md5, buf.md5);
-              if (response.buf_content_get) {
-                log.warn("lengths: db: %s file: %s", _.size(response.buf_content_get), _.size(result));
-              }
-              return cb(null, false);
-            });
-          }];
-
-          auto.read_s3 = ["read_buf", function (cb, response) {
-            if (response.read_buf || response.verify_buf) {
-              checksum_matches++;
-              return cb();
-            }
-            if (!s3_client) {
-              stats.bufs.empty++;
-              save_buf_content(ws, buf, "");
-              return cb();
-            }
-            s3_key = util.format("%s/%s", db_room.id, buf.fid);
-            log.debug("Fetching %s from s3.", s3_key);
-            load_s3(s3_key, function (err, result) {
-              if (err) {
-                log.error("Error reading %s from s3: %s", s3_key, err);
-                stats.bufs.empty++;
-                save_buf_content(ws, buf, "");
-              } else {
-                save_buf_content(ws, buf, result);
-              }
-              cb(err);
-            });
-          }];
-
-          async.auto(auto, function (err) {
-            if (err) {
-              stats.bufs.failed++;
-            }
-            cb();
-          });
-        }, function (err) {
-          var version_key = util.format("version_%s", db_room.id);
-          if (err) {
-            server_db.put(version_key, -1, function () {
-              ws.end();
-            });
-          } else {
-            server_db.put(version_key, checksum_matches, function () {
-              ws.end();
-            });
-          }
-        });
-      });
-    });
   });
 };
 
