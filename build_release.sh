a1465dab2ce65d82bed3ea587a673a4a991ae13b
Geoff Greer <geoff@greer.fm>
2014-07-03 12:29:39 -0700

    Update build and deploy scripts to no longer depend on npm. Use local settings. Add settings migration script. Ignore local_settings.

--- build_release.sh
+++ build_release.sh
@@ -3,6 +3,7 @@
 DATETIME=`date -u '+%Y_%m_%d_%H%M'`
 RELEASE_NAME="colab-$DATETIME"
 
-echo "$RELEASE_NAME.tar.gz"
+FILE_LIST=`git ls-files`
 
-tar -c -z -f $RELEASE_NAME.tar.gz `git ls-files`
+tar -c -z -f $RELEASE_NAME.tar.gz $FILE_LIST node_modules && \
+echo "$RELEASE_NAME.tar.gz"
