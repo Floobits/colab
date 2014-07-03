1bdb208d35242783657ff9dbd1ab2d42ebda6c2b
Geoff Greer <geoff@greer.fm>
2014-07-03 13:25:20 -0700

    Archive symlink targets when building releases.

--- build_release.sh
+++ build_release.sh
@@ -5,5 +5,5 @@ RELEASE_NAME="colab-$DATETIME"
 
 FILE_LIST=`git ls-files`
 
-tar -c -z -f $RELEASE_NAME.tar.gz $FILE_LIST node_modules && \
+tar -c -z -L -f $RELEASE_NAME.tar.gz $FILE_LIST node_modules && \
 echo "$RELEASE_NAME.tar.gz"
