7eebf20d141e24db352d6efb1af7d6911879169d
Geoff Greer <geoff@greer.fm>
2014-07-03 13:24:50 -0700

    Forgot to git add the upgrade script.

---
+++ upgrade.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+  echo "Usage: $0 release-name.tar.gz"
+  exit 0
+fi
+
+TARBALL=$1
+RELEASE_NAME=$(basename "$TARBALL")
+RELEASE_NAME="${RELEASE_NAME%%.*}"
+RELEASE_DIR="/data/releases/$RELEASE_NAME"
+
+mkdir $RELEASE_DIR && \
+tar xzf $TARBALL --directory $RELEASE_DIR && \
+cd $RELEASE_DIR && \
+npm rebuild && \
+node ./lib/migrate_settings.js /data/colab/lib && \
+cp /data/colab/lib/local_settings.js $RELEASE_DIR/lib/local_settings.js && \
+ln -s -f $RELEASE_DIR /data/colab-new && \
+mv -T -f /data/colab-new /data/colab && \
+sv restart /service/colab && \
+echo "Successfully updated to $RELEASE_NAME"
