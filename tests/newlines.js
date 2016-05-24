94df8261f5900a1a2485c6f6d876e54556ef463d
Geoff Greer <geoff@greer.fm>
2016-05-23 18:13:18 -0700

    Use patch/pop correctly in newline test.

--- tests/newlines.js
+++ tests/newlines.js
@@ -6,12 +6,12 @@ const verify = test.verify;
 const agents = test.agents;
 
 function crs(t) {
-  // agents[0].buf = "1234\n5678\n9012\n3456\n";
-  // agents[1].buf = "1234\n5678\n9012\n3456\n";
-
   patch(agents[0], "1234\n5678\n9012\n3456\n");
-  patch(agents[0], "1234\r\n5678\n9012\n3456\n");
+  patch(agents[1], "1234\n5678\n9012\n3456\n");
+  agents[0].pop_patch(-1);
+  agents[1].pop_patch(-1);
 
+  patch(agents[0], "1234\r\n5678\n9012\n3456\n");
   agents[0].pop_patch(-1);
 
   verify(t, agents);
