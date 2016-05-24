e1a9fd35c6e0ac60178abb277ab244bf4d432cb5
Geoff Greer <geoff@greer.fm>
2016-05-23 19:08:12 -0700

    Whoops. Forgot to apply patches at the end of these two tests. Enable indentation tests now that they pass.

--- tests/indentation.js
+++ tests/indentation.js
@@ -16,6 +16,7 @@ function tabs(t) {
 
   patch(agents[0], "blah\n\t  blah\n\tblah\n");
   agents[0].pop_patch(-1);
+  agents[1].pop_patch(-1);
 
   t.deepEqual(agents[0].events.error, [{
     "msg": util.format("Possible indentation mismatch! %s is indented with spaces, but you sent tabs. Check your indentation rules!", basename(agents[0].buf.path)),
